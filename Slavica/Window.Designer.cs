01c02b353f4d6935a59b8872526eb61fc25cf682
Ridireacht <ridiredire@gmail.com>
2022-09-07 20:29:04 +0300

    Added some language columns

--- Slavica/Window.Designer.cs
+++ Slavica/Window.Designer.cs
@@ -34,11 +34,32 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
+            this.label20 = new System.Windows.Forms.Label();
+            this.label21 = new System.Windows.Forms.Label();
+            this.label22 = new System.Windows.Forms.Label();
+            this.label23 = new System.Windows.Forms.Label();
+            this.label24 = new System.Windows.Forms.Label();
+            this.label25 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(540, 61);
+            this.button1.Location = new System.Drawing.Point(540, 63);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(98, 30);
             this.button1.TabIndex = 0;
@@ -65,7 +86,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Bahnschrift", 12.096F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(64, 136);
+            this.label2.Location = new System.Drawing.Point(50, 136);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(120, 25);
             this.label2.TabIndex = 3;
@@ -75,7 +96,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Bahnschrift", 12.096F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(540, 136);
+            this.label3.Location = new System.Drawing.Point(558, 136);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(116, 25);
             this.label3.TabIndex = 4;
@@ -91,11 +112,221 @@
             this.label4.TabIndex = 5;
             this.label4.Text = "South Slavic";
             // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(18, 187);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 21);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Russian:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(18, 217);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(80, 21);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Ukrainian:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(18, 249);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(85, 21);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Belarusian:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(18, 281);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(56, 21);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Rusyn:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(18, 312);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(96, 21);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "Old Russian:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(280, 312);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(114, 21);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "Lower Sorbian:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(280, 281);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(114, 21);
+            this.label11.TabIndex = 14;
+            this.label11.Text = "Upper Sorbian:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(280, 249);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(66, 21);
+            this.label12.TabIndex = 13;
+            this.label12.Text = "Silesian:";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(280, 217);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(85, 21);
+            this.label13.TabIndex = 12;
+            this.label13.Text = "Kashubian:";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(280, 187);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(54, 21);
+            this.label14.TabIndex = 11;
+            this.label14.Text = "Polish:";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(280, 407);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(59, 21);
+            this.label15.TabIndex = 18;
+            this.label15.Text = "Slovak:";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(280, 376);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(54, 21);
+            this.label16.TabIndex = 17;
+            this.label16.Text = "Czech:";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(280, 344);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(72, 21);
+            this.label17.TabIndex = 16;
+            this.label17.Text = "Polabian:";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Location = new System.Drawing.Point(535, 407);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(125, 21);
+            this.label18.TabIndex = 26;
+            this.label18.Text = "Church Slavonic:";
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.Location = new System.Drawing.Point(535, 376);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(79, 21);
+            this.label19.TabIndex = 25;
+            this.label19.Text = "Bulgarian:";
+            // 
+            // label20
+            // 
+            this.label20.AutoSize = true;
+            this.label20.Location = new System.Drawing.Point(535, 344);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(98, 21);
+            this.label20.TabIndex = 24;
+            this.label20.Text = "Macedonian:";
+            // 
+            // label21
+            // 
+            this.label21.AutoSize = true;
+            this.label21.Location = new System.Drawing.Point(535, 312);
+            this.label21.Name = "label21";
+            this.label21.Size = new System.Drawing.Size(68, 21);
+            this.label21.TabIndex = 23;
+            this.label21.Text = "Bosnian:";
+            // 
+            // label22
+            // 
+            this.label22.AutoSize = true;
+            this.label22.Location = new System.Drawing.Point(535, 281);
+            this.label22.Name = "label22";
+            this.label22.Size = new System.Drawing.Size(110, 21);
+            this.label22.TabIndex = 22;
+            this.label22.Text = "Serbocroatian:";
+            // 
+            // label23
+            // 
+            this.label23.AutoSize = true;
+            this.label23.Location = new System.Drawing.Point(535, 249);
+            this.label23.Name = "label23";
+            this.label23.Size = new System.Drawing.Size(66, 21);
+            this.label23.TabIndex = 21;
+            this.label23.Text = "Serbian:";
+            // 
+            // label24
+            // 
+            this.label24.AutoSize = true;
+            this.label24.Location = new System.Drawing.Point(535, 217);
+            this.label24.Name = "label24";
+            this.label24.Size = new System.Drawing.Size(72, 21);
+            this.label24.TabIndex = 20;
+            this.label24.Text = "Croatian:";
+            // 
+            // label25
+            // 
+            this.label25.AutoSize = true;
+            this.label25.Location = new System.Drawing.Point(535, 187);
+            this.label25.Name = "label25";
+            this.label25.Size = new System.Drawing.Size(81, 21);
+            this.label25.TabIndex = 19;
+            this.label25.Text = "Slovenian:";
+            // 
             // Window
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.label19);
+            this.Controls.Add(this.label20);
+            this.Controls.Add(this.label21);
+            this.Controls.Add(this.label22);
+            this.Controls.Add(this.label23);
+            this.Controls.Add(this.label24);
+            this.Controls.Add(this.label25);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -117,5 +348,26 @@
         private Label label2;
         private Label label3;
         private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private Label label13;
+        private Label label14;
+        private Label label15;
+        private Label label16;
+        private Label label17;
+        private Label label18;
+        private Label label19;
+        private Label label20;
+        private Label label21;
+        private Label label22;
+        private Label label23;
+        private Label label24;
+        private Label label25;
     }
 }
