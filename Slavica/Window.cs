9f42aa0ee0e1814a47072a28f40c04650d186fe0
Ridireacht <ridiredire@gmail.com>
2022-09-07 20:58:24 +0300

    Minor

--- Slavica/Window.cs
+++ Slavica/Window.cs
@@ -16,10 +16,18 @@ namespace Slavica
             // turns off the 'proceed' button if the entry is incorrect
             if (textBox1.Text == "")
                 button1.Enabled = false;
+
             else if (Regex.IsMatch(textBox1.Text, "^[a-zA-Z0-9]*$"))
                 button1.Enabled = true;
             else
                 button1.Enabled = false;
+        }
+
+
+        // starts a translation process
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Logic.Start();
         }
     }
 }
