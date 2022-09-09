40b628f5fb57dca9b1ac03f6dec6b69e53284be4
Ridireacht <ridiredire@gmail.com>
2022-09-09 19:39:02 +0300

    Some basic preparations

---
+++ Slavica/Scrapper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slavica
+{
+    internal class Scrapper
+    {
+        string api = "https://iapi.glosbe.com/iapi3/";
+        string url;
+
+
+        void Get(string keyword)
+        {
+            url = api + 
+        }
+
+
+        // changes word form to the URL one
+        string wordToURL(string word)
+        {
+            return null;
+        }
+    }
+}
