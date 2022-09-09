39d97578b3dc162d1d69e29a88c6c7ee14f5ab09
Ridireacht <ridiredire@gmail.com>
2022-09-09 19:46:16 +0300

    Added some lists

--- Slavica/Scrapper.cs
+++ Slavica/Scrapper.cs
@@ -9,12 +9,16 @@ namespace Slavica
     internal class Scrapper
     {
         string api = "https://iapi.glosbe.com/iapi3/";
-        string url;
-
+        List<string> urls = new List<string>();
+        List<string> languages = new List<string>() { "ru", "be", "uk", "rue", "orv", "pl", "csb", "szl",
+                                                      "hsb", "dsb", "pox", "cs", "sk", "sl", "hr",
+                                                      "sr", "sh", "bs", "mk", "bg", "cu" };
 
         void Get(string keyword)
         {
-            url = api + 
+            keyword = wordToURL(keyword);
+
+
         }
 
 
