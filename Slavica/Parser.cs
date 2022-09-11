e9ec2bd98595cb99fd71bdb6092132c36c65e1ee
Ridireacht <ridiredire@gmail.com>
2022-09-11 14:58:49 +0300

    Code optimization

--- Slavica/Parser.cs
+++ Slavica/Parser.cs
@@ -14,20 +14,13 @@ namespace Slavica
             string temp;
 
 
-            // Regex patterns preset
-            Regex rgx1 = new Regex(@"\,|\{|\}|\[|\]|\:|\: True', '");
-            Regex rgx2 = new Regex(@"\' \'', '\'");
-            Regex rgx3 = new Regex(@" True', '");
-            Regex rgx4 = new Regex(@"^\'|\'$', '");
-
-
             foreach (string content in contents)
             {
                 // clearing the content
-                temp = rgx1.Replace(content, replacement);
-                temp = rgx2.Replace(temp, replacement);
-                temp = rgx3.Replace(temp, replacement);
-                temp = rgx4.Replace(temp, replacement);
+                temp = Regex.Replace(content, @"\,|\{|\}|\[|\]|\:|\: True', '", "");
+                temp = Regex.Replace(temp, @"\' \'', '\'", "");
+                temp = Regex.Replace(temp, @" True', '", "");
+                temp = Regex.Replace(temp, @"^\'|\'$', '", "");
 
                 temp = Regex.Replace(temp, "after", "");
                 temp = Regex.Replace(temp, "phrase", "");
