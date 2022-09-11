c3257e0c51d3c05540e28f88ecbb4b17f7422dc6
Ridireacht <ridiredire@gmail.com>
2022-09-11 14:50:05 +0300

    Added two another layers

--- Slavica/Parser.cs
+++ Slavica/Parser.cs
@@ -16,7 +16,9 @@ namespace Slavica
 
             // Regex patterns preset
             Regex rgx1 = new Regex(",|{|}|[|]|:|: True");
-            Regex rgx2 = new Regex("' '', ''");
+            Regex rgx2 = new Regex(" '', '");
+            Regex rgx3 = new Regex(" True', '");
+            Regex rgx4 = new Regex("^'|'$', '");
 
 
             foreach (string content in contents)
@@ -24,6 +26,13 @@ namespace Slavica
                 // clearing the content
                 temp = rgx1.Replace(content, replacement);
                 temp = rgx2.Replace(temp, replacement);
+                temp = rgx3.Replace(temp, replacement);
+                temp = rgx4.Replace(temp, replacement);
+
+                temp = Regex.Replace(temp, "after", "");
+                temp = Regex.Replace(temp, "phrase", "");
+                temp = Regex.Replace(temp, "translations", "");
+                temp = Regex.Replace(temp, "success", "");
 
                 words.Add(temp);
             }
