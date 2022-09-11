9aca006ed7de89830961ecd46853043e30ed4373
Ridireacht <ridiredire@gmail.com>
2022-09-11 15:10:37 +0300

    Kinda (barely) working raw prototype

--- Slavica/Parser.cs
+++ Slavica/Parser.cs
@@ -10,7 +10,6 @@ namespace Slavica
         public static List<string> ParseInfo(List<string> contents)
         {
             List<string> words = new();
-            const string replacement = "";
             string temp;
 
 
@@ -26,6 +25,11 @@ namespace Slavica
                 temp = Regex.Replace(temp, "phrase", "");
                 temp = Regex.Replace(temp, "translations", "");
                 temp = Regex.Replace(temp, "success", "");
+
+                temp = Regex.Replace(temp, "\"+", "\"");
+
+                var tete = Regex.Split(temp, "\"");
+                temp = tete[2] + " / " + tete[3];
 
                 words.Add(temp);
             }
